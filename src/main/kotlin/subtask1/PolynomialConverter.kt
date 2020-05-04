764cff20597d2536e8b807b48d6b0de118920a78
Nikolai24 <61163795+Nikolai24@users.noreply.github.com>
2020-05-04 23:28:15 +0300

    Update PolynomialConverter.kt

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -2,8 +2,42 @@ package subtask1
 
 class PolynomialConverter {
 
-    // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+        if (numbers.isEmpty()) {
+            return (null)
+        }
+        var n = numbers.size - 1
+        var s = ""
+        var c: String
+        var k = 1
+        for (element in numbers) {
+            c = ""
+            if (n == numbers.size - 1) {
+                c = "$element"
+            }  else {
+                if (element > 0) {
+                    c = " + $element"
+                }
+                else {
+                    if (element < 0) {
+                        k = element * -1
+                        if (k == 1) {
+                            c = " - "
+                        } else {
+                            c = " - $k"
+                        }
+                    }
+                }
+            }
+            if ((n > 1) && (element != 0)) {
+                c += "x^$n"
+            }
+            if ((n == 1) && (element != 0)) {
+                c += "x"
+            }
+            s += c
+            n--
+        }
+        return (s)
     }
 }
