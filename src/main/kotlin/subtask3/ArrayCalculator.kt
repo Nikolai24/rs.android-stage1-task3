92b4f065cb89dfaa472131c0f71d12e04a876014
Nikolai24 <61163795+Nikolai24@users.noreply.github.com>
2020-05-05 18:48:42 +0300

    Update ArrayCalculator.kt

--- src/main/kotlin/subtask3/ArrayCalculator.kt
+++ src/main/kotlin/subtask3/ArrayCalculator.kt
@@ -2,8 +2,31 @@ package subtask3
 
 class ArrayCalculator {
 
-    // TODO: Complete the following function
     fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
-        throw NotImplementedError("Not implemented")
+        val newArray = itemsFromArray.filterIsInstance<Int>().toTypedArray().sortedArray()
+        if (newArray.isEmpty()) {
+            return 0
+        }
+        var n = numberOfItems
+        if (numberOfItems > newArray.size) {
+            n = newArray.size
+        }
+        var s = newArray.size - 1
+        var result = 1
+        var c = 0
+        for (i in 1 .. n) {
+            if (i > n) {
+                return result
+            }
+            if ((n-i > 0) && (newArray[c]*newArray[c+1] > newArray[s]*newArray[s-1])) {
+                result *= newArray[c] * newArray[c+1]
+                n--
+                c++
+            } else {
+                result *=  newArray[s]
+                s--
+            }
+        }
+        return result
     }
 }
