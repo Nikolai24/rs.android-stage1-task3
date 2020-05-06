98c1f0a7880b514709fcfae9078e6fa3f5ba87b1
Nikolai24 <61163795+Nikolai24@users.noreply.github.com>
2020-05-06 20:16:34 +0300

    Update FullBinaryTrees.kt

--- src/main/kotlin/subtask6/FullBinaryTrees.kt
+++ src/main/kotlin/subtask6/FullBinaryTrees.kt
@@ -2,8 +2,24 @@ package subtask6
 
 class FullBinaryTrees {
 
-    // TODO: Complete the following function
     fun stringForNodeCount(count: Int): String {
-        throw NotImplementedError("Not implemented")
+        if(count==1) return "[[0]]"
+        if(count%2==0) return "[]"
+        var result ="["
+        for (c: String in getList("[0", count-1, 0)) {
+            result+=c
+        }
+        result+="]"
+        return result
+    }
+    private fun getList(root: String, v: Int, node: Int): ArrayList<String>{
+        if (v==0) return arrayListOf("$root]")
+        else {
+            val array: ArrayList<String> = getList("$root,0,0", v - 2, node+1)
+            if (node>0) {
+                array.addAll(getList("$root,null,null", v, node-1))
+            }
+            return array
+        }
     }
 }
